45810023e48529eea6416ef6ce8dd07de1247c05
XIuming Lee <lxmcoder@gmail.com>
2019-10-19 12:39:03 +0800

    init...

---
+++ boot-jfx/src/main/java/cn/xiuminglee/bootjfx/BootJfxApplication.java
@@ -0,0 +1,13 @@
+package cn.xiuminglee.bootjfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJfxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJfxApplication.class, args);
+    }
+
+}
