52dff5ab00a49c6ea849dca24129b70e03c2ae69
XIuming Lee <lxmcoder@gmail.com>
2019-10-21 09:40:02 +0800

    Spring Boot和JavaFX整合参考分支

--- boot-jfx/src/main/java/cn/xiuminglee/bootjfx/JavaFxApp.java
+++ boot-jfx/src/main/java/cn/xiuminglee/bootjfx/JavaFxApp.java
@@ -17,7 +17,7 @@ import org.springframework.core.io.ClassPathResource;
  * @Author: Xiuming Lee
  * @Date: 2019/10/19 12:50
  * @Version 1.0
- * @Describe: JavaFX的主类
+ * @Describe: JavaFX的主类，可将此类抽出为抽象类。
  */
 public class JavaFxApp extends Application {
     private ConfigurableApplicationContext context;
