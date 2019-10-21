51be3136cb71f8020df13b03fd12ab510e1d1b2f
XIuming Lee <lxmcoder@gmail.com>
2019-10-21 09:43:03 +0800

    master分支

--- boot-jfx/src/main/java/cn/xiuminglee/bootjfx/BootJfxApplication.java
+++ boot-jfx/src/main/java/cn/xiuminglee/bootjfx/BootJfxApplication.java
@@ -3,10 +3,12 @@ package cn.xiuminglee.bootjfx;
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication
 public class BootJfxApplication {
     public static void main(String[] args) {
         Application.launch(JavaFxApp.class,args);
     }
-
 }
